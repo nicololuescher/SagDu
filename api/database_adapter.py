654ca132a3761ffe506f49ba9d7cc8cd8a697e9d
Nicolo Lüscher <nicolo.luescher@natron.io>
2025-08-22 23:25:16 +0200

    added datatypes

---
+++ api/database_adapter.py
@@ -0,0 +1,10 @@
+class DatabaseAdapter:
+    def __init__(self) -> None:
+        pass
+    def connect(self) -> None:
+        pass
+    def disconnect(self) -> None:
+        pass
+    def execute_query(self, query: str) -> None:
+        pass
+    
